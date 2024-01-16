503565794b53c3206918c6f29878ea2ddb904ca8
Amit Bhatia <abhatia@ips-yes.com>
2024-01-16 13:20:40 -0500

    adding a new program sum of array

---
+++ Hello/sum_of_array.java
@@ -0,0 +1,26 @@
+public class sum_of_array {
+    //import java.lang.Math
+
+    public static void main(String[] args) 
+    {
+        String str1 = "sum_of_array";
+        String str2 = new String("sum_of_array");
+        if(str1 == str2) {
+            System.out.println("str1 == str2");
+        } else {
+            System.out.println("str1 != str2");
+        }
+
+        if(str1.equals(str2)) {
+            System.out.println("str1 equals str2");
+        } else {
+            System.out.println("str1 does not equals str2");
+        }
+
+        /*System.out.println("sum_of_array World");
+        for(int i = 0 ; i<= 10; i++) {
+            System.out.println("2^(" + i + ") = " + Math.pow(2,i));
+        }
+        */
+    }
+}
