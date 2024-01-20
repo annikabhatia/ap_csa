9fc8954d1c0e0a2a1c6b18e94979970109e5ef90
Amit Bhatia <abhatia@ips-yes.com>
2024-01-19 20:41:54 -0500

    code to find maximum and minimum number in array

---
+++ Hello/max_and_min.java
@@ -0,0 +1,18 @@
+public class max_and_min {
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, -2, 49, 34 };
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (max > arr[i])
+                max = arr[i];
+            if (min < arr[i])
+                min = arr[i];
+        }
+
+        System.out.println("The maxiumum value is: " + max);
+        System.out.println("The minimum value is: " + min);
+    }
+}
