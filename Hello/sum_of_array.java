11ee6cc1026191c706020efc1d4c92451acd18b2
Amit Bhatia <abhatia@ips-yes.com>
2024-01-19 20:38:23 -0500

    sum of array program

--- Hello/sum_of_array.java
+++ Hello/sum_of_array.java
@@ -3,24 +3,15 @@ public class sum_of_array {
 
     public static void main(String[] args) 
     {
-        String str1 = "sum_of_array";
-        String str2 = new String("sum_of_array");
-        if(str1 == str2) {
-            System.out.println("sum_of_array str1 == str2");
-        } else {
-            System.out.println("sum_of_array str1 != str2");
-        }
+       int sum = 0;
+       int[] arr = {5, 8, 45, 32, 67};
+       
+       for (int i = 0; i < arr.length; i++) {
+        sum+=arr[i];
+       }
 
-        if(str1.equals(str2)) {
-            System.out.println("str1 equals str2");
-        } else {
-            System.out.println("str1 does not equals str2");
-        }
+       System.out.println(sum);
 
-        /*System.out.println("sum_of_array World");
-        for(int i = 0 ; i<= 10; i++) {
-            System.out.println("2^(" + i + ") = " + Math.pow(2,i));
-        }
-        */
+    return;
     }
 }
