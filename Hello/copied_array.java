04f3e4573aa88ca8880e138ccfa4b0da44f00970
Amit Bhatia <abhatia@ips-yes.com>
2024-01-28 11:03:35 -0500

    copy elements from one array to another array

---
+++ Hello/copied_array.java
@@ -0,0 +1,25 @@
+public class copied_array {
+    public static void print_array(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i]);
+            System.out.print(", ");    
+        }
+        System.out.println("");
+    }
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr2 = new int[arr.length]; 
+
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = arr[i];
+        }
+
+        print_array(arr);
+        print_array(arr2);
+        //System.out.println(arr);
+        //System.out.println(arr2);   
+    }
+}
+
+
+// need to check how to run this
