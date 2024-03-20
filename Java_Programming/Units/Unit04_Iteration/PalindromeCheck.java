3d802593a5a1c9557cf5ba77c9fb2349d72bb038
Amit Bhatia <abhatia@ips-yes.com>
2024-03-19 21:51:26 -0400

    palindrome code for String

---
+++ Java_Programming/Units/Unit04_Iteration/PalindromeCheck.java
@@ -0,0 +1,22 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam"));
+        System.out.println(isPalindrome("noon"));
+        System.out.println(isPalindrome("annika"));
+        System.out.println(isPalindrome("rotator"));
+    }
+
+    public static boolean isPalindrome(String input) {
+        int n = input.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (input.charAt(i) != input.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+        }
+            
+    }
+
+
+
